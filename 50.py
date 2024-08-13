eeca3ed4ce32bfd6b2f8f324a8891062e543e408
Chandresh Kumar <chandresh.code@gmail.com>
2024-08-13 18:27:15 +0530

    feat: add problem #50 solution

---
+++ 50.py
@@ -0,0 +1,24 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        ans = x
+        if n == 0:
+            return 1
+        elif n == 1:
+            return x
+        else:
+            if n < 0:
+                return 1/self.myPow(x, -1*n)
+            if n%2 == 0:
+                return self.myPow(x*x, int(n/2))
+            else:
+                return x*self.myPow(x*x, int((n-1)/2))
+
+
+if __name__ == "__main__":
+    x, n = 2.0, 10
+    
+    print(Solution().myPow(x, n)) # 1024
+    
+    x, n = 2.0, -2
+    
+    print(Solution().myPow(x, n)) # 0.25
